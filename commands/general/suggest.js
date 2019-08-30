5252f27eb46429d49df9fa2c183172d16bdfeb86
ArkaneOnline <niflheim.exease@gmail.com>
2019-08-30 09:31:00 -0500

    add suggest

---
+++ commands/general/suggest.js
@@ -0,0 +1,27 @@
+const { RichEmbed } = require("discord.js");
+const config = require("../../config.json");
+
+module.exports = {
+    config: {
+        name: "suggest",
+        aliases: [],
+        descpription: "A command to suggest various things to the server",
+        category: "general"
+    },
+    run: async (client, message, args) => {
+        let suggestion = args.join(" ");
+        if(!suggestion) return message.reply("Tell me what you want to suggest! `/suggest <suggestion>`");
+
+        let suggestembed = new RichEmbed()
+        .setTitle("Suggestion")
+        .setAuthor(message.author)
+        .setColor(config.colors.purple)
+        .setDescription(suggestion)
+
+        let suggestionchannel = message.guild.channels.find(`name`, "suggesitons");
+        suggestionchannel.send(suggestembed);
+
+        return message.reply("Your suggestion has been sent!");
+
+    }
+}
