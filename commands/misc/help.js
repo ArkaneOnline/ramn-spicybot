50aef96cb444d335d68d526446e0d20df4aeb6d0
ArkaneOnline <niflheim.exease@gmail.com>
2019-08-29 19:58:31 -0500

    fix crash

---
+++ commands/misc/help.js
@@ -0,0 +1,11 @@
+module.exports = {
+    config: {
+        name: "help",
+        aliases: [],
+        description: "The help command",
+        category: "misc"
+    },
+    run: async (client, message, args) => {
+        return;
+    }
+}
