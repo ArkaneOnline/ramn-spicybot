cfc026f254041783177352535fce872653fa49a5
ArkaneOnline <niflheim.exease@gmail.com>
2019-08-30 10:46:56 -0500

    scanrole command

---
+++ commands/misc/scanroles.js
@@ -0,0 +1,28 @@
+module.exports = {
+    config: {
+        name: "scanroles",
+        aliases: [],
+        description: "A command that will scan all of the users roles",
+        category: "misc"
+    },
+    run: async (client, message, args) => {
+        await message.delete();
+
+        message.channel.send("Scanning process initiated...");
+
+        //scanning code here
+        let clan1role = message.guild.roles.find(`name`, "Clan 1 Member");
+        let clan2role = message.guild.roles.find(`name`, "Clan 2 Member");
+        let guestrole = message.guild.roles.find(`name`, "Guest");
+
+        const ramenshop = client.guilds.get("511289361178820618");
+        ramenshop.members.foreach(member => {
+            if(member.roles.has(clan1role)) member.removeRole(guestrole);
+            if(member.roles.has(clan2role)) member.removeRole(guestrole);
+            if(!member.roles.has(clan1role)) member.addRole(guestrole)
+            if(!member.roles.has(clan2role)) member.addRole(guestrole)
+        })
+
+        message.edit("Scanning process completed!");
+    }
+}
