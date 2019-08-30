d39635f4bb6091e6af96425273c4b2a7ecda8abb
ArkaneOnline <niflheim.exease@gmail.com>
2019-08-30 11:04:44 -0500

    add timing

--- commands/misc/scanroles.js
+++ commands/misc/scanroles.js
@@ -17,12 +17,14 @@ module.exports = {
 
         const ramenshop = client.guilds.get("511289361178820618");
         ramenshop.members.forEach(member => {
+            const delay = (msec) => new Promise((resolve) => setTimeout(resolve, msec));
+            await delay(2000)
             if(member.roles.has(clan1role)) member.removeRole(guestrole);
             if(member.roles.has(clan2role)) member.removeRole(guestrole);
             if(!member.roles.has(clan1role)) member.addRole(guestrole)
             if(!member.roles.has(clan2role)) member.addRole(guestrole)
             if(message.member.bot) member.removeRole(guestrole);
-        }, 2000)
+        })
 
         message.channel.send("Scanning process complete!");
     }
