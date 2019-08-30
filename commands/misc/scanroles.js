c9ad36ceae0908b1261c476ae33dce058bd2b524
ArkaneOnline <niflheim.exease@gmail.com>
2019-08-30 10:51:06 -0500

    time

--- commands/misc/scanroles.js
+++ commands/misc/scanroles.js
@@ -17,6 +17,7 @@ module.exports = {
 
         const ramenshop = client.guilds.get("511289361178820618");
         ramenshop.members.forEach(member => {
+            await(2000);
             if(member.roles.has(clan1role)) member.removeRole(guestrole);
             if(member.roles.has(clan2role)) member.removeRole(guestrole);
             if(!member.roles.has(clan1role)) member.addRole(guestrole)
