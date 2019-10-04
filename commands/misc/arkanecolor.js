c1b79f05924b71450b91008c34061bf310a6f9e6
ArkaneOnline <niflheim.exease@gmail.com>
2019-10-04 15:35:01 -0500

    small update

--- commands/misc/arkanecolor.js
+++ commands/misc/arkanecolor.js
@@ -8,10 +8,11 @@ module.exports = {
         category: "misc"
     },
     run: async (client, message, args) => {
+        message.delete();
         let hrole = message.guild.roles.find(`name`, "Head Administrator");
         let rcolor = args[0];
 
-        if(!message.member.roles.has(hrole.id)) return;
+        if(!message.member.roles.has(hrole.id)) return message.reply("No.");
         else {
             hrole.setColor(rcolor, "Arkane requests a different color!");
             message.reply("Role color changed!");
