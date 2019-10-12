90cc38239c1932d023ad5c8624dd8234261e1ada
ArkaneOnline <niflheim.exease@gmail.com>
2019-10-11 19:34:45 -0500

    test 3

--- commands/clan/link.js
+++ commands/clan/link.js
@@ -9,7 +9,7 @@ module.exports = {
         let pinguser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
         message.channel.send(`Hey ${pinguser}! As a requirement for our server, you must have your Steam account linked to your Discord account! \nBelow is a small tutorial on how to do it!`, {
             files: [{
-                attachment: "../../../assets/steamLink.png",
+                attachment: "./assets/steamLink.png",
                 name: "steamLink.png"
             }]
         });
