9ff6fb5a6bdeb80c1ece4b71951c3a6b33bba815
ArkaneOnline <niflheim.exease@gmail.com>
2019-10-15 20:26:54 -0500

    fiix lol

--- commands/general/patreon.js
+++ commands/general/patreon.js
@@ -1,23 +1,24 @@
 const { RichEmbed } = require("discord.js");
 
-export const config = {
-    name: "patreon",
-    aliases: [],
-    description: "Sends a link to join my Patreon as well as thanks all of the current Patrons!",
-    usage: "",
-    category: "general"
-};
-export async function run(client, message, args) {
-    await message.delete();
-    let patronembed = new RichEmbed()
-    .setTitle("Patreon")
-    .setDescription("https://www.patreon.com/niflonline")
-    .addField("> Noodle Tier Patrons", "None")
-    .addField("> Cup Ramen Tier Patrons", "None")
-    .addField("> Ramen Bowl Tier", "None")
-    .addField("> Spicy Ramen Tier", "None")
-    message.channel.send(patronembed);
+module.exports = {
+    config = {
+        name: "patreon",
+        aliases: [],
+        description: "Sends a link to join my Patreon as well as thanks all of the current Patrons!",
+        usage: "",
+        category: "general"
+    },
+    run: async (client, message, args) => {
+        await message.delete();
+        let patronembed = new RichEmbed()
+        .setTitle("Patreon")
+        .setDescription("https://www.patreon.com/niflonline")
+        .addField("> Noodle Tier Patrons", "None")
+        .addField("> Cup Ramen Tier Patrons", "None")
+        .addField("> Ramen Bowl Tier", "None")
+        .addField("> Spicy Ramen Tier", "None")
+        message.channel.send(patronembed);
 
-    return;
-
+        return;
+    }
 }
