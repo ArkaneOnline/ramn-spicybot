9fa01006da86837cbffbbf7be4fa75436c038068
ArkaneOnline <niflheim.exease@gmail.com>
2019-10-17 15:37:06 -0500

    server info

---
+++ commands/general/serverinfo.js
@@ -0,0 +1,25 @@
+const { RichEmbed } = require("discord.js");
+const config = require("../../config.json");
+
+module.exports = {
+    config: {
+        name: "serverinfo",
+        aliases: [],
+        description: "A command to display the info of the current server",
+        usage: "",
+        category: "general"
+    },
+    run: async (client, message, args) => {
+        let serverembed = new RichEmbed()
+        .setTitle(message.guild.name)
+        .setColor(config.yellow)
+        .setThumbnail(message.guild.icon)
+        .setURL("https://invite.gg/ramenshop")
+        .addField("Server ID", message.guild.id)
+        .addField("Created on", message.guild.createdAt)
+        .addField("Member count", message.guild.memberCount)
+        .addField("Owner", message.guild.owner)
+        .setTimestamp()
+        .setFooter(`Info requested by: ${message.author.username}`, message.author.displayAvatarURL)
+    }
+}
