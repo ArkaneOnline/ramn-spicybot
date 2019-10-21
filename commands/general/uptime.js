6a5472bb9b691545810b20ddd1d8b10388fbcd88
ArkaneOnline <niflheim.exease@gmail.com>
2019-10-21 11:47:24 -0500

    ez

--- commands/general/uptime.js
+++ commands/general/uptime.js
@@ -12,7 +12,7 @@ module.exports = {
         let hours = Math.floor(totalSeconds / 3600);
         totalSeconds %= 3600;
         let minutes = Math.floor(totalSeconds / 60);
-        let seconds = totalSeconds % 60;
+        let seconds = Math.floor(totalSeconds % 60);
     
         let uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
         message.channel.send(uptime);
