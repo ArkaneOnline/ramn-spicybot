99133127f541634f8eb36589220043af3d2cfdac
ArkaneOnline <niflheim.exease@gmail.com>
2019-10-22 15:56:52 -0500

    add say

---
+++ commands/utils/say.js
@@ -0,0 +1,16 @@
+module.exports = {
+    config: {
+        name: "say",
+        aliases: [],
+        description: "Says whatever you tell me to",
+        usage: "<text>",
+        category: "util"
+    },
+    run: async (client, message, args) => {
+        let say = args.join(" ");
+        if(!say) return message.reply("I need to know what you want me to say! `/help say`");
+
+        message.channel.send(say);
+        return;
+    }
+}
