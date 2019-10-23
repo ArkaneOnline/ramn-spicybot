0796d244e6d9b8b8eaeeb3782c2c8d20037e068c
ArkaneOnline <niflheim.exease@gmail.com>
2019-10-23 10:05:23 -0500

    error check

--- commands/clan/joinclan.js
+++ commands/clan/joinclan.js
@@ -1,3 +1,8 @@
+const Traveler = require("the-traveler").default;
+const Enums = require("the-traveler/build/enums")
+
+const traveler = new Traveler({ apikey: "b91083e79056494d91ca582eaaad9938", userAgent: "app-platform" })
+
 module.exports = {
     config: {
         name: "joinclan",
@@ -8,6 +13,9 @@ module.exports = {
     },
     run: async (client, message, args) => {
         await message.delete();
+        let clan3count = traveler.groupv2.getMembersOfGroup("1", "3868416")
+
+        consolg.log(clan3count);
 
         //oh god i can't wait to format this
         message.channel.send("We have a few select clans that you can join! \nBelow are a list of clans that we run, so pick you clan and bug your clan staff in <#616735734874898462>! \n\nClan 1: \nhttps://www.bungie.net/en/ClanV2?groupid=2089890\n\nClan 2: \nhttps://www.bungie.net/en/ClanV2?groupid=3778179\n\nClan 3: \nhttps://www.bungie.net/en/ClanV2?groupid=3868416\n\nWe hope you enjoy your stay in our clan! \nYou can DM a Staff Member or ask in <#511289361178820621> if you have any further questions!");
