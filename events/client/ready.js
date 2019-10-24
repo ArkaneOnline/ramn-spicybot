80de6214cb37bf6afdde2ddc479f323048f99893
ArkaneOnline <niflheim.exease@gmail.com>
2019-10-23 19:34:52 -0500

    revert ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -2,21 +2,18 @@ const Discord = require("discord.js");
 const { prefix } = require("../../config.json");
 
 module.exports = async (client, message) => {
-    let omegalul = client.emojis.get("591452570103578644");
-    
     let presences = [
-        //"Running through Titan baricades since 1989",
-        //"It's real ape hours",
-        //`${client.users.size} Ramen Shop members`,
-        //"Bubble Titans are scary Titans",
-        //"Join the Ramen Shop!",
-        //"011011100111010101110100",
-        //"Read the rules!",
-        //"Hunter main, big brain",
-        //"Invite your friends!",
-        //"Titan main, tiny brain",
-        //"Can we get an async for each function?",
-        `side${omegalul}rms`
+        "Running through Titan baricades since 1989",
+        "It's real ape hours",
+        `${client.users.size} Ramen Shop members`,
+        "Bubble Titans are scary Titans",
+        "Join the Ramen Shop!",
+        "011011100111010101110100",
+        "Read the rules!",
+        "Hunter main, big brain",
+        "Invite your friends!",
+        "Titan main, tiny brain",
+        "Can we get an async for each function?",
     ]
     setInterval(() => {
         const presence = Math.floor(Math.random() * (presences.length - 1) + 1);
