ee810a77d9874804ece57feaad507da437c60fb8
ArkaneOnline <niflheim.exease@gmail.com>
2019-10-25 12:19:36 -0500

    4head move lol

--- commands/utils/steam.js
+++ commands/utils/steam.js
@@ -7,7 +7,7 @@ module.exports = {
         category: "utils"
     },
     run: async (client, message, args) => {
-        if(!message.member.hasPermission("MANAGE_MESSAGE")) return message.reply("No.");
+        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
         let pinguser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
         message.channel.send(`Hey ${pinguser}! As a requirement for our server, you must have your Steam account linked to your Discord account! \nBelow is a small tutorial on how to do it!`, {
             files: [{
