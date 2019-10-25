fa64b7883b056a413aebe9890677ec4cc0001144
ArkaneOnline <niflheim.exease@gmail.com>
2019-10-25 14:50:04 -0500

    remove manage messages dependency for steam command

--- commands/utils/steam.js
+++ commands/utils/steam.js
@@ -3,18 +3,27 @@ module.exports = {
         name: "steam",
         aliases: ["link"],
         description: "A guide on how to link your Steam Profile to Discord",
-        usage: "<@user>",
+        usage: "(@user)",
         category: "utils"
     },
     run: async (client, message, args) => {
-        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
         let pinguser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-        message.channel.send(`Hey ${pinguser}! As a requirement for our server, you must have your Steam account linked to your Discord account! \nBelow is a small tutorial on how to do it!`, {
-            files: [{
-                attachment: "./assets/steamLink.png",
-                name: "steamLink.png"
-            }]
-        });
-        return;
+        if(pinguser){
+            message.channel.send(`Hey ${pinguser}! As a requirement for our server, you must have your Steam account linked to your Discord account! \nBelow is a small tutorial on how to do it!`, {
+                files: [{
+                    attachment: "./assets/steamLink.png",
+                    name: "steamLink.png"
+                }]
+            });
+            return;
+        } else {
+            message.channel.send(`Hello! As a requirement for our server, you must have your Steam account linked to your Discord account! \nBelow is a small tutorial on how to do it!`, {
+                files: [{
+                    attachment: "./assets/steamLink.png",
+                    name: "steamLink.png"
+                }]
+            });
+            return;
+        }
     }
 }
