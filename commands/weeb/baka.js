4a99b8513805d72a0c524cff828b89173dc1ffed
ArkaneOnline <niflheim.exease@gmail.com>
2019-10-28 12:51:08 -0500

    revert es6

--- commands/weeb/baka.js
+++ commands/weeb/baka.js
@@ -1,15 +1,18 @@
-import client from "nekos.life";
+const client = require("nekos.life");
 const neko = new client();
 
-export const config = {
-    name: "baka",
-    aliases: [],
-    description: "Baka!",
-    usage: "",
-    category: "weeb"
-};
-export async function run(client, message, args) {
-    let baka = await neko.sfw.baka();
-    message.channel.send(`Baka! ${baka.url}`);
-    return;
+module.exports = {
+    config: {
+        name: "baka",
+        aliases: [],
+        description: "Baka!",
+        usage: "",
+        category: "weeb"
+    },
+    run: async (client, message, args) => {
+        let baka = await neko.sfw.baka();
+        message.channel.send(`Baka! ${baka.url}`);
+
+        return;
+    }
 }
