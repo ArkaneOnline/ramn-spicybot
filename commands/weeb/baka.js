f3ef8a5492fa0153bc7bea45e175ee57af34cc52
ArkaneOnline <niflheim.exease@gmail.com>
2019-11-05 10:28:09 -0600

    110519

--- commands/weeb/baka.js
+++ commands/weeb/baka.js
@@ -1,4 +1,6 @@
+//requires all the packages needed for this
 const client = require("nekos.life");
+//creates a new instance of a nekos.life api client
 const neko = new client();
 
 module.exports = {
@@ -10,9 +12,12 @@ module.exports = {
         category: "weeb"
     },
     run: async (client, message, args) => {
+        //defines json data into a variable
         let baka = await neko.sfw.baka();
+        //sends json data as a link
         message.channel.send(`Baka! ${baka.url}`);
 
+        //stops the code
         return;
     }
 }
--- commands/weeb/poke.js
+++ commands/weeb/poke.js
@@ -1,4 +1,6 @@
+//requires all the packages needed for this
 const client = require("nekos.life");
+//creates a new instance of a nekos.life api client
 const neko = new client();
 
 module.exports = {
@@ -10,15 +12,21 @@ module.exports = {
         category: "weeb"
     },
     run: async (client, message, args) => {
+        //defines json data into a variable
         let poke = await neko.sfw.poke();
+        //defines a user that we ping as a variable
         let puser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
 
+        //checks if we specified a user
         if(puser){
+            //sends json data in a message as a link
             message.channel.send(`${puser}, Poke! \n${poke.url}`);
         } else {
+            //sends json data in a message as a link
             message.channel.send(`Poke! \n${poke.url}`);
         }
 
+        //stops the code
         return;
     }
 }
