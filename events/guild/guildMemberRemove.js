c6703b4398aa68ff91ae9fe909ed3efec7eb4b54
ArkaneOnline <niflheim.exease@gmail.com>
2019-11-05 22:09:09 -0600

    update guildMemberRemove

--- events/guild/guildMemberRemove.js
+++ events/guild/guildMemberRemove.js
@@ -3,9 +3,9 @@ const { prefix } = require("../../config.json");
 module.exports = async (client, member) => {
     const welcomeChannel = member.guild.channels.find(x => x.name === "welcome");
     const messages = [
-        `${member.username} left!`,
-        `${member.username}, Guardian Down!`,
-        `${member.username} is gone!`
+        `${member.user.username} left!`,
+        `${member.user.username}, Guardian Down!`,
+        `${member.user.username} is gone!`
     ];
     if(!welcomeChannel) return;
     const message = Math.floor(Math.random() * (messages.length - 1) + 1);
