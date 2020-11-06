e5e143ce52746651bb63113cf38b085e9e16ebb7
ArkaneOnline <niflheim.exease@gmail.com>
2020-11-06 00:32:13 -0600

    update lfg command

--- commands/destiny/lfg.js
+++ commands/destiny/lfg.js
@@ -10,11 +10,11 @@ module.exports = {
         let lfgrole = message.guild.roles.cache.find(role => role.name === "LFG");
 
         if (!message.member.roles.cache.has(lfgrole)) {
-            message.member.roles.remove(lfgrole, "Spicy Bot automatic role assign. Self Assigned user role.")
-            message.reply("I have removed the `LFG` role from you!");
-        } else {
             message.member.roles.add(lfgrole, "Spicy Bot automatic role assign. Self Assigned user role.")
-            message.reply("I have added the `LFG` role to you!")
+            message.reply("I have added the `LFG` role to you!");
+        } else {
+            message.member.roles.remove(lfgrole, "Spicy Bot automatic role assign. Self Assigned user role.")
+            message.reply("I have removed the `LFG` role from you!")
         }
 
         return;
