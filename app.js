80eb52831fe3e56a9eeb661f8b0794e3fb22a8e3
ArkaneOnline <niflheim.exease@gmail.com>
2020-11-06 01:33:51 -0600

    test automatic xur location message

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const { Client, Collection } = require("discord.js");
+const cron = require("cron");
 const client = new Client();
+
+let xurMessage = new cron.CronJob('00 05 12 * * 5', () => {
+    let ramenShop = client.guilds.cache.get("511289361178820618");
+    let resetChannel = ramenShop.channels.cache.get("548999830048079875");
+
+    resetChannel.send("https://wherethefuckisxur.com");
+}, null, true, 'America/Chicago');
+
+xurMessage.start();
 
 ["commands", "aliases"].forEach(x => client[x] = new Collection());
 ["command", "event"].forEach(x => require(`./handlers/${x}`)(client));
