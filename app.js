2ad704fdc8881d2037ed60f5a50edd83bab147c9
ArkaneOnline <32213487+ArkaneOnline@users.noreply.github.com>
2020-11-14 03:34:17 -0600

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ let xurMessage = new cron.CronJob('00 05 11 * * 5', () => {
 let unregisteredMessage = new cron.CronJob('00 00 15 * * *', () => {
     var announcementChannel = ramenShop.channels.cache.get("511922224727457804");
     let unregisteredRole = ramenShop.roles.cache.get("776988950929473596");
-    announcementChannel.send(`${unregisteredRole}, please make sure you head over to #commands and type \`d!register\`. \nCharlemange will DM you with a link to link your Destiny and Discord accounts together!`);
+    announcementChannel.send(`${unregisteredRole}, please make sure you head over to #commands and type \`d!register\`. \nCharlemange will DM you with a link to link your Destiny and Discord accounts together! \nIf you have already registered with Charlemange, your accounts may have been desynced and might require you to register again. This can happen from time to time, but all your latest Destiny information will be fetched. \nOnce you have successfully registered with Charlemange, you will receive the \`Registered Members\` role.`);
 }, null, true, 'America/Chicago');
 
 xurMessage.start();
