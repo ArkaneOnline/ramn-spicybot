b3afff0dd4439388c3dc9b9ad1c622ef2b15b3b2
ArkaneOnline <32213487+ArkaneOnline@users.noreply.github.com>
2020-11-13 21:29:14 -0600

    update dm message

--- events/guild/message.js
+++ events/guild/message.js
@@ -3,7 +3,9 @@ const { MessageEmbed } = require("discord.js");
 
 module.exports = async (client, message) => {
     let messageEmbed = new MessageEmbed()
-        .setDescription(`I can only be used in the Ramen Shop.`)
+        .setTitle(`Error!`)
+        .setDescription(`I cannot be used in DMs. \nIf you need to use me, go to The Spiciest Ramen Shop to do so. \nIf you are not a member of The Spiciest Ramen Shop, you do not need to interact with me.`)
+        .setThumbnail(client.user.avatarURL({ format: "png", dynamic: true }))    
         .setColor(colors.darkblue);
 
     if(message.author.bot) return;
