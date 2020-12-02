98f9744ad8f30d6a6d72d0c2b84801035d70b09d
ArkaneOnline <32213487+ArkaneOnline@users.noreply.github.com>
2020-12-01 20:17:42 -0600

    Update app.js

--- app.js
+++ app.js
@@ -8,14 +8,13 @@ let xurMessage = new cron.CronJob('00 10 11 * * 5', () => {
     var resetChannel = ramenShop.channels.cache.get("548999830048079875");
     resetChannel.send("https://wherethefuckisxur.com");
 }, null, true, 'America/Chicago');
+xurMessage.start();
 
 let unregisteredMessage = new cron.CronJob('00 00 15 * * 0-6', () => {
     var announcementChannel = ramenShop.channels.cache.get("511922224727457804");
     let unregisteredRole = ramenShop.roles.cache.get("776988950929473596");
     announcementChannel.send(`${unregisteredRole}, please make sure you head over to #commands and type \`d!register\`. \nCharlemange will DM you with a link to link your Destiny and Discord accounts together! \nIf you have already registered with Charlemange, your accounts may have been desynced and might require you to register again. This can happen from time to time, but all your latest Destiny information will be fetched. \nOnce you have successfully registered with Charlemange, you will receive the \`Registered Members\` role.`);
 }, null, true, 'America/Chicago');
-
-xurMessage.start();
 unregisteredMessage.start();
 
 ["commands", "aliases"].forEach(x => client[x] = new Collection());
