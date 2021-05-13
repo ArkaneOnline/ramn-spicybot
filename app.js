acd9364955ce829c203d0b52424db646bf530794
ArkaneOnline <niflheim.exease@gmail.com>
2021-05-13 11:55:54 -0500

    update where is xur message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const { Client, Collection } = require("discord.js");
 const cron = require("cron");
 const client = new Client();
 
-let xurMessage = new cron.CronJob('00 10 11 * * 5', () => {
+let xurMessage = new cron.CronJob('00 10 12 * * 5', () => {
     var ramenShop = client.guilds.cache.get("511289361178820618");
     var resetChannel = ramenShop.channels.cache.get("548999830048079875");
     resetChannel.send("https://wherethefuckisxur.com");
