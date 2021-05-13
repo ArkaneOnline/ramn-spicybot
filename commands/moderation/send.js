7a417697077ffb0aceceac8540580054d17a9555
ArkaneOnline <niflheim.exease@gmail.com>
2021-05-13 11:54:38 -0500

    test send command

---
+++ commands/moderation/send.js
@@ -0,0 +1,21 @@
+const config = require("../../config.json");
+
+module.exports = {
+    config: {
+        name: "send",
+        aliases: [],
+        description: "Sends a message to a user",
+        category: "moderation"
+    },
+    run: async (client, message, args) => {
+        if(!message.author.hasPermission("ADMINISTRATOR")) return message.reply("Insufficient Permissions: Administrator");
+        if(!args[0]) return message.reply(`Incorrect Usage: \`${prefix}send @user message\``);
+
+        let user = args[0];
+        let sendmessage = args.join(" ").slice(22);
+
+        user.send(sendmessage);
+
+        return;
+    }
+}
