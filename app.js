466211ad05019c58c94c65b5b9983676220a3e5d
ArkaneOnline <niflheim.exease@gmail.com>
2021-05-14 11:40:58 -0500

    remove not working code

--- app.js
+++ app.js
@@ -9,14 +9,6 @@ let xurMessage = new cron.CronJob('00 10 12 * * 5', () => {
 }, null, true, 'America/Chicago');
 xurMessage.start();
 
-let unregisteredMessage = new cron.CronJob('00 00 15 * * 0-6', () => {
-    var ramenShop = client.guilds.cache.get("511289361178820618");
-    var announcementChannel = ramenShop.channels.cache.get("511922224727457804");
-    let unregisteredRole = ramenShop.roles.cache.get("776988950929473596");
-    announcementChannel.send(`<@${unregisteredRole.id}>, please make sure you head over to #commands and type \`d!register\`. \nCharlemange will DM you with a link to link your Destiny and Discord accounts together! \nIf you have already registered with Charlemange, your accounts may have been desynced and might require you to register again. This can happen from time to time, but all your latest Destiny information will be fetched. \nOnce you have successfully registered with Charlemange, you will receive the \`Registered Members\` role.`);
-}, null, true, 'America/Chicago');
-unregisteredMessage.start();
-
 ["commands", "aliases"].forEach(x => client[x] = new Collection());
 ["command", "event"].forEach(x => require(`./handlers/${x}`)(client));
 
