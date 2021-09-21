5e1b5ee527751441e3f59b1ed666b0e54305121f
Arkane <arkane@wservers.tk>
2021-09-21 04:23:17 -0500

    time to test and see what the fuck i just made

--- src/commands/utilities/poll.js
+++ src/commands/utilities/poll.js
@@ -20,7 +20,119 @@ module.exports = {
             content: "You do not have permission to perform this action",
             ephemeral: true
         })
+        
+        //add choices and message to variables
         let content = interaction.options.getString("content");
+        let choice1 = interaction.options.getString("choice1");
+        let choice2 = interaction.options.getString("choice2");
+        let choice3 = interaction.options.getString("choice3");
+        let choice4 = interaction.options.getString("choice4");
+        let choice5 = interaction.options.getString("choice5");
+        let choice6 = interaction.options.getString("choice6");
+        let choice7 = interaction.options.getString("choice7");
+        let choice8 = interaction.options.getString("choice8");
+        let choice9 = interaction.options.getString("choice9");
+
+        //doing the illegal for checking purposes
+        if(!choice3){
+            if(!choice4){
+                if(!choice5){
+                    if(!choice6){
+                        if(!choice7){
+                            if(!choice8){
+                                if(!choice9){
+                                    console.log("Poll with no additional choiced created");
+                                } else {
+                                    if(!choice3){
+                                        choice3 = choice9;
+                                    } else {
+                                        if(!choice4){
+                                            choice4 = choice9;
+                                        } else {
+                                            if(!choice5){
+                                                choice5 = choice9;
+                                            } else {
+                                                if(choice6){
+                                                    choice6 = choice9;
+                                                } else {
+                                                    if(!choice7){
+                                                        choice7 = choice9;
+                                                    } else {
+                                                        choice8 = choice9;
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            } else {
+                                if(!choice3){
+                                    choice3 = choice8;
+                                } else {
+                                    if(!choice4){
+                                        choice4 = choice8;
+                                    } else {
+                                        if(!choice5){
+                                            choice5 = choice8;
+                                        } else {
+                                            if(!choice6){
+                                                choice6 = choice8;
+                                            } else {
+                                                choice7 = choice8;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        } else {
+                            if(!choice3){
+                                choice3 = choice7;
+                            } else {
+                                if(!choice4){
+                                    choice4 = choice7;
+                                } else {
+                                    if(!choice5){
+                                        choice5 = choice7;
+                                    } else {
+                                        choice6 = choice7;
+                                    }
+                                }
+                            }
+                        }
+                    } else {
+                        if(!choice3){
+                            choice3 = choice6;
+                        } else {
+                            if(!choice4){
+                                choice4 = choice6;
+                            } else {
+                                choice5 = choice6;
+                            }
+                        }
+                    }
+                } else {
+                    if(!choice3){
+                        choice3 = choice5;
+                    } else {
+                        choice5 = choice4;
+                    }
+                }
+            } else {
+                choice3 = choice4;
+            }
+        }
+
+        //testing
+        if(choice1) console.log(choice1);
+        if(choice2) console.log(choice2);
+        if(choice3) console.log(choice3);
+        if(choice4) console.log(choice4);
+        if(choice5) console.log(choice5);
+        if(choice6) console.log(choice6);
+        if(choice7) console.log(choice7);
+        if(choice8) console.log(choice8);
+        if(choice9) console.log(choice9);
+
         let pollembed = new MessageEmbed().setTitle("Poll").setDescription(`${content}`).setColor("BLURPLE").setTimestamp();
         const msg = await interaction.channel.send({ embeds: [pollembed] });
         msg.react("✅");
