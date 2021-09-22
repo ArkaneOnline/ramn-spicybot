c46fb4a0363d27f58ac862fffa4e1bd01d2f56a4
ArkaneOnline <niflheim.exease@gmail.com>
2021-09-21 19:25:04 -0500

    rearrange

--- src/app.js
+++ src/app.js
@@ -10,6 +10,16 @@ const functions = fs.readdirSync("./src/functions").filter(file => file.endsWith
 const eventFiles = fs.readdirSync("./src/events").filter(file => file.endsWith(".js"));
 const commandFolders = fs.readdirSync("./src/commands");
 
+(async () => {
+    for (file of functions) {
+        require(`./functions/${file}`)(client);
+    }
+
+    client.handleEvents(eventFiles, "./src/events");
+    client.handleCommands(commandFolders, "./src/commands");
+    client.login(process.env.TOKEN);
+})();
+
 //twitter integration
 //twitter configuration
 const Twitter = require("twit");
@@ -38,16 +48,6 @@ stream.on("tweet", tweet => {
     client.channels.cache.get(destinationChannelID).send(twitterMessage);
 })
 //end twitter integration
-
-(async () => {
-    for (file of functions) {
-        require(`./functions/${file}`)(client);
-    }
-
-    client.handleEvents(eventFiles, "./src/events");
-    client.handleCommands(commandFolders, "./src/commands");
-    client.login(process.env.TOKEN);
-})();
 
 //continue this video from 2:30
 //https://www.youtube.com/watch?v=HNH4V6Dhw6s
