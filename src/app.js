aed784f727e03577093aa7b2f67c482b8c40b0a0
ArkaneOnline <niflheim.exease@gmail.com>
2021-09-21 19:19:27 -0500

    add twitter integration

--- src/app.js
+++ src/app.js
@@ -10,6 +10,35 @@ const functions = fs.readdirSync("./src/functions").filter(file => file.endsWith
 const eventFiles = fs.readdirSync("./src/events").filter(file => file.endsWith(".js"));
 const commandFolders = fs.readdirSync("./src/commands");
 
+//twitter integration
+//twitter configuration
+const Twitter = require("twit");
+const twitterConf = {
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+}
+const twitterClient = new Twitter(twitterConf);
+const destinationChannelID = "774146090731307018"
+//end twitter configuration
+
+const stream = twitterClient.stream("statuses/filter", {
+    follow: '1440464833141301259',
+});
+
+stream.on("tweet", tweet => {
+    if (tweet.retweeted_status
+        || tweet.in_reply_to_status_id
+        || tweet.in_reply_to_status_id_str
+        || tweet.in_reply_to_user_id
+        || tweet.in_reply_to_user_id_str
+        || tweet.in_reply_to_screen_name) return;
+    const twitterMessage = `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
+    client.channels.cache.get(destinationChannelID).send(twitterMessage);
+})
+//end twitter integration
+
 (async () => {
     for (file of functions) {
         require(`./functions/${file}`)(client);
