898557b35c72b68a674bbafada774bdeb7391fe6
Arkane <arkane@wservers.tk>
2021-09-24 04:08:17 -0500

    im either a complete dumbass or a fucking genius

---
+++ src/events/presenceUpdate.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: "presenceUpdate",
+    async execute(newPresence, client) {
+        let serverID = "511289361178820618";
+        let channelID = "774146090731307018";
+
+        if(newPresence.activities.type === "STREAMING") {
+            let streamer = newPresence.member;
+            let link = newPresence.activities.url;
+
+            let guild = client.guilds.cache.get(serverID);
+            let channel = guild.channels.cache.get(channelID);
+
+            channel.send(`${streamer} has gone live at: ${link}`);
+        }
+    }
+}
