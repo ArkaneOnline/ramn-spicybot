2fc27c002a87ada0e8294809e4babbe17fa3621e
Arkane <arkane@wservers.tk>
2021-09-25 03:00:34 -0500

    testing

--- src/events/presenceUpdate.js
+++ src/events/presenceUpdate.js
@@ -2,27 +2,11 @@ module.exports = {
     name: "presenceUpdate",
     async execute(oldPresence, newPresence, client) {
         console.log("User Presence Cahnged!");
-        let serverID = "511289361178820618";
-        let channelID = "774146090731307018";
-
-        if(newPresence.activities.type == "STREAMING") {
-            let streamer = newPresence.member;
-            let link = newPresence.activities.url;
-
-            let guild = client.guilds.cache.get(serverID);
-            let channel = guild.channels.cache.get(channelID);
-
-            channel.send(`${streamer} has gone live at: ${link}`);
-        }
-
-        if(newPresence.activities.type == "PLAYING") {
-            let userPlaying = newPresence.member;
-            let content = newPresence.activities.name;
-
-            let guild = client.guilds.cache.get(serverID);
-            let channel = guild.channels.cache.get(channelID);
-
-            channel.send(`Testing: ${userPlaying.tag} is now playing ${content}`);
-        }
+        if (!newPresence.activities) return false;
+        newPresence.activities.forEach(activity => {
+            if (activity.type == "PLAYING") {
+                console.log(`${newPresence.user.tag} is playing a game.`);
+            };
+        });
     }
 }
