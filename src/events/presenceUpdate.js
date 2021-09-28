40d117e8576cd274215cd146f587118c4adad547
ArkaneOnline <niflheim.exease@gmail.com>
2021-09-27 20:41:56 -0500

    testing

--- src/events/presenceUpdate.js
+++ src/events/presenceUpdate.js
@@ -5,13 +5,14 @@ module.exports = {
         let clanrole = newPresence.guild.roles.cache.get("892213185749319760");
 
         if (!newPresence.activities) return false;
-        if (newPresence.member.roles.cache.has(clanrole)) return console.log("User does not have RAMN role.");
-        newPresence.activities.forEach(activity => {
-            if (oldPresence.activities.type == "STREAMING") return false;
-            if (activity.type == "STREAMING") {
-                channel.send(`${newPresence.user.tag} is now live! \nChack them out at: **<${activity.url}>**`);
-                message.delete();
-            };
-        });
+        if (newPresence.member.roles.cache.has(clanrole)) {
+            newPresence.activities.forEach(activity => {
+                if (oldPresence.activities.type == "STREAMING") return false;
+                if (activity.type == "STREAMING") {
+                    channel.send(`${newPresence.user.tag} is now live! \nChack them out at: **<${activity.url}>**`);
+                };
+            });
+        }
+        await message.delete();
     }
 }
