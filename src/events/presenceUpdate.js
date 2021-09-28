8d001c35413f4e2831a484980ef2ffc12a215d9b
Arkane <arkane@wservers.tk>
2021-09-27 19:59:11 -0500

    testing

--- src/events/presenceUpdate.js
+++ src/events/presenceUpdate.js
@@ -2,13 +2,12 @@ module.exports = {
     name: "presenceUpdate",
     async execute(oldPresence, newPresence, client) {
         let channel = client.channels.cache.get("891238696223899668");
-        let clan1role = newPresence.guild.roles.cache.get("588843272860991498");
-        let clan2role = newPresence.guild.roles.cache.get("588843161690964023");
+        let clanrole = newPresence.guild.roles.cache.get("892213185749319760");
 
         if (!newPresence.activities) return false;
-        if (!newPresence.member.roles.cache.has(clan1role || clan2role)) return;
+        if (!newPresence.member.roles.cache.has(clanrole)) return false;
         newPresence.activities.forEach(activity => {
-            if (oldPresence.activities.type == "STREAMING") return;
+            if (oldPresence.activities.type == "STREAMING") return false;
             if (activity.type == "STREAMING") {
                 channel.send(`${newPresence.user.tag} is now live! \nChack them out at: **<${activity.url}>**`);
             };
