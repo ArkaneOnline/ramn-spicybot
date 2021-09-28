8a7ca15c694d39207739942328f80bd7f5e1a89e
ArkaneOnline <niflheim.exease@gmail.com>
2021-09-27 20:26:38 -0500

    more tests

--- src/events/presenceUpdate.js
+++ src/events/presenceUpdate.js
@@ -5,11 +5,12 @@ module.exports = {
         let clanrole = newPresence.guild.roles.cache.get("892213185749319760");
 
         if (!newPresence.activities) return false;
-        // if (!newPresence.member.roles.cache.has(clanrole)) return false;
         newPresence.activities.forEach(activity => {
+            // if (!newPresence.member.roles.cache.has(clanrole)) return false;
             // if (oldPresence.activities.type == "STREAMING") return false;
             if (activity.type == "STREAMING") {
                 channel.send(`${newPresence.user.tag} is now live! \nChack them out at: **<${activity.url}>**`);
+                await message.delete();
             };
         });
     }
