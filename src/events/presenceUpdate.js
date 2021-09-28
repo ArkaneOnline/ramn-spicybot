e1cf37bf7e97d2fed3451e6ac0a8eecb72954dd7
ArkaneOnline <niflheim.exease@gmail.com>
2021-09-27 20:15:04 -0500

    more tests

--- src/events/presenceUpdate.js
+++ src/events/presenceUpdate.js
@@ -7,7 +7,7 @@ module.exports = {
         if (!newPresence.activities) return false;
         // if (!newPresence.member.roles.cache.has(clanrole)) return false;
         newPresence.activities.forEach(activity => {
-            // if (oldPresence.activities.type == "STREAMING") return false;
+            if (oldPresence.activities.type == "STREAMING") return false;
             if (activity.type == "STREAMING") {
                 channel.send(`${newPresence.user.tag} is now live! \nChack them out at: **<${activity.url}>**`);
             };
