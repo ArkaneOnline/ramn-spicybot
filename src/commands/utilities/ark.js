ef37292a5ca50e45e3e4254876f1c5895723d9a7
Arkane <niflheim.exease@gmail.com>
2022-01-15 00:53:33 -0600

    another fix

--- src/commands/utilities/ark.js
+++ src/commands/utilities/ark.js
@@ -5,20 +5,28 @@ module.exports = {
         .setName("ark")
         .setDescription("Allows you to assign the ARK Updates role to yourself")
         .addStringOption(option => 
-            option.setName("add/remove")
+            option.setName("option")
                 .setDescription("Choose whether to add or remove the role")
                 .setRequired(true)
                 .addChoice("Add", "add")
                 .addChoice("Remove", "remove")),
 
     async execute(interaction) {
-        let choice = interaction.options.getString("Add/Remove");
+        let choice = interaction.options.getString("option");
         let arkrole = interaction.guild.roles.cache.get("931801378693722132");
 
         if(choice === "add") {
             interaction.member.roles.add(arkrole, "Spicybot Self assignable role");
+            interaction.reply({
+                content: "The **ARK Updates** role has been added to you!",
+                ephemeral: true
+            });
         } else if(choice === "remove") {
             interaction.member.roles.remove(arkrole, "Spicybot Self assignable role");
+            interaction.reply({
+                content: "The **ARK Updates** role has been removed from you!",
+                ephemeral: true
+            });
         }
     }
 }
