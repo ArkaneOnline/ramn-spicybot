3b79f8c30cc3c1f94f1ef04c2033f506334f73c9
Arkane <niflheim.exease@gmail.com>
2022-01-15 00:49:45 -0600

    add ark role assign

---
+++ src/commands/utilities/ark.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("ark")
+        .setDescription("Allows you to assign the ARK Updates role to yourself")
+        .addStringOption(option => 
+            option.setName("Add/Remove")
+                .setDescription("Choose whether to add or remove the role")
+                .setRequired(true)
+                .addChoice("Add", "add")
+                .addChoice("Remove", "remove")),
+
+    async execute(interaction) {
+        let choice = interaction.options.getString("Add/Remove");
+        let arkrole = interaction.guild.roles.cache.get("931801378693722132");
+
+        if(choice === "add") {
+            interaction.member.roles.add(arkrole, "Spicybot Self assignable role");
+        } else if(choice === "remove") {
+            interaction.member.roles.remove(arkrole, "Spicybot Self assignable role");
+        }
+    }
+}
