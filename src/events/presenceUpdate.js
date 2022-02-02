15be3cc06d52bdb75926b69423c6ab18ac5274aa
Arkane <arkane@wservers.tk>
2022-02-01 22:43:20 -0600

    debug league ban

--- src/events/presenceUpdate.js
+++ src/events/presenceUpdate.js
@@ -4,7 +4,8 @@ module.exports = {
         let banChannel = client.channels.cache.get("560340160819232788");
 
         if (!newPresence.activities) return;
-        newPresence(activity => {
+        console.log(newPresence.activities)
+        newPresence.activities(activity => {
             if (activity.name === "League of Legends") {
                 newPresence.guild.member.ban({ days: 7, reason: "Playing League of Legends" });
                 banChannel.send(`${newPresence.member.user.tag} has been banned for playing League of Legends.`);
