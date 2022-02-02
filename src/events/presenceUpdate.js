95f43d267ce2c56dad98ab3a9ec990277c034ddf
Arkane <arkane@wservers.tk>
2022-02-01 22:38:29 -0600

    bug fix league banning feature

--- src/events/presenceUpdate.js
+++ src/events/presenceUpdate.js
@@ -4,9 +4,11 @@ module.exports = {
         let banChannel = client.channels.cache.get("560340160819232788");
 
         if (!newPresence.activities) return;
-        if (newPresence.activities.name("League of Legends")) {
-            newPresence.guild.member.ban({ days: 7, reason: "Playing League of Legends" });
-            banChannel.send(`${newPresence.member.user.tag} has been banned for playing League of Legends.`)
-        }
+        newPresence.activities(activity => {
+            if (activity.name === "League of Legends") {
+                newPresence.guild.member.ban({ days: 7, reason: "Playing League of Legends" });
+                banChannel.send(`${newPresence.member.user.tag} has been banned for playing League of Legends.`);
+            }
+        })
     }
 }
