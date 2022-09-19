25e3f556c65dcc6bca3c1dd9109983f31810ed92
ArkaneOnline <32213487+ArkaneOnline@users.noreply.github.com>
2022-09-19 18:58:03 -0500

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "ready",
     once: true,
     async execute(client) {
-        client.user.setPresence({ activities: [{ name: `ramnclan.tk` }], status: "dnd" })
+        client.user.setPresence({ activities: [{ name: `ramnclan.ga` }], status: "dnd" })
         console.log(`\n${client.user.username} is online!`);
     }
 }
