490cd71110573621ae5c95886d95216ac797d8ea
ArkaneOnline <32213487+ArkaneOnline@users.noreply.github.com>
2022-11-25 01:59:41 -0600

    remove twitter

--- src/app.js
+++ src/app.js
@@ -21,40 +21,9 @@ const commandFolders = fs.readdirSync("./src/commands");
     client.login(process.env.TOKEN);
 })();
 
-//twitter integration
-//twitter configuration
-const Twitter = require("twit");
-const twitterConf = {
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-}
-const twitterClient = new Twitter(twitterConf);
-const destinationChannelID = "511922224727457804"
-//end twitter configuration
-
-const stream = twitterClient.stream("statuses/filter", {
-    follow: '1440464833141301259',
-});
-
-stream.on("tweet", tweet => {
-    if (tweet.retweeted_status
-        || tweet.in_reply_to_status_id
-        || tweet.in_reply_to_status_id_str
-        || tweet.in_reply_to_user_id
-        || tweet.in_reply_to_user_id_str
-        || tweet.in_reply_to_screen_name) return;
-    const twitterMessage = `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
-    client.channels.cache.get(destinationChannelID).send(twitterMessage);
-})
-//end twitter integration
-
 var timer = 5, interval = timer * 60 * 1000; //This checks every 5 minutes, change 5 to whatever minute you'd like
 setInterval(function() {
 	fetch("https://betteruptime.com/api/v1/heartbeat/VvUhsKD7Z72vkpAaHbpaegNc")
     console.log("Spicybot: GET request made to heartbeat.");
 }, interval);
 
-//continue this video from 2:30
-//https://www.youtube.com/watch?v=HNH4V6Dhw6s
